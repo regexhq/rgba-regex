c425e20b2f44e8b6ff1c8770519905d597d29c76
Tyler Waters <tswaters@users.noreply.github.com>
2018-12-01 18:08:07 -0800

    fix: make it work with percentages, closes #1
    Fixes #1 
    
    Also need to update CI because mocha doesn't run on oldnode anymore 😄

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ module.exports = function rgbaRegex(options) {
   options = options || {};
 
   return options.exact ?
-    /^rgba\((\d{1,3}),\s*(\d{1,3}),\s*(\d{1,3}),\s*(\d*(?:\.\d+)?)\)$/ :
-    /rgba\((\d{1,3}),\s*(\d{1,3}),\s*(\d{1,3}),\s*(\d*(?:\.\d+)?)\)/ig;
+    /^rgba\((\d{1,3}%?),\s*(\d{1,3}%?),\s*(\d{1,3}%?),\s*(\d*(?:\.\d+)?)\)$/ :
+    /rgba\((\d{1,3}%?),\s*(\d{1,3}%?),\s*(\d{1,3}%?),\s*(\d*(?:\.\d+)?)\)/ig;
 }
